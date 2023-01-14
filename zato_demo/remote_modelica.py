112c7c62a8d84d4e078afea0d32461a04a5c2dfa
John DeHart <jdehart@avian.com>
2023-01-14 08:59:55 -0500

    Added some cool sysmlv2 models

--- zato_demo/remote_modelica.py
+++ zato_demo/remote_modelica.py
@@ -25,12 +25,17 @@ class GetUserDetails(Service):
         freq = self.request.input.Freq
 
         # A command
-        command = f'curl -k -X GET http://3.236.246.39:10100/scilab_xcos_model?Amp={amp}&Freq={freq}'
-        #command = f'curl http://127.0.0.1:10100/scilab_xcos_model?Amp={amp}&Freq={freq}'
-        #command = "curl -X GET http://www.google.com" 
+        command = f'curl -k -X GET http://54.208.197.217:10100/scilab_xcos_model?Amp={amp}&Freq={freq}'
 
+            # Here is what a generic service might look like
+            #command = f'curl -k -X GET http://{hostIp}:{port}}/{scriptName}?args={args}'
+
+        
         # We can send the response to a pubsub :)
         response = self.commands.invoke(command, callback='/commands/results', use_pubsub=True)
+
+            # Here is a generic topic that can be pushed along with the command data
+            #response = f'self.commands.invoke(command, callback="{topicName}", use_pubsub=True)'
 
         # Write the response to the caller
         self.response.payload = {"Response":response.stdout, "Is OK?":response.is_ok}
